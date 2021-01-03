5d22b6b3a4f21e26f52b50dec67448f876ac2250
GnosticOccultist <41146201+GnosticOccultist@users.noreply.github.com>
2021-01-03 16:45:52 +0100

    Improvements on VertexAttribute
    * Use stride and offset also when attribute is panning along multiple locations.

--- mercury-nucleus/src/fr/mercury/nucleus/renderer/opengl/vertex/VertexAttribute.java
+++ mercury-nucleus/src/fr/mercury/nucleus/renderer/opengl/vertex/VertexAttribute.java
@@ -12,7 +12,7 @@ public class VertexAttribute {
 	 * The location for an unknown attribute.
 	 */
 	public static final int UNKNOWN_LOCATION = -1;
-	
+
 	/**
 	 * The name of the attribute.
 	 */
@@ -34,7 +34,7 @@ public class VertexAttribute {
 	 */
 	private int offset = 0;
 	/**
-	 * The divisor to modify the rate at which the attribute advance during 
+	 * The divisor to modify the rate at which the attribute advance during
 	 * instanced rendering.
 	 */
 	private int divisor = 0;
@@ -42,23 +42,23 @@ public class VertexAttribute {
 	 * The span of the attribute.
 	 */
 	private int span = 1;
-	
+
 	public VertexAttribute(String name, int location) {
 		this(name, location, 0, 0);
 	}
-	
+
 	public VertexAttribute(String name, int location, int stride, int offset) {
 		this(name, null, location, stride, offset, 0, 1);
 	}
-	
+
 	public VertexAttribute(String name, String bufferType, int location, int stride, int offset, int divisor, int span) {
 		Validator.nonEmpty(name, "The name of the attribute can't be empty or null!");
-		Validator.inRange(location, 0, Integer.MAX_VALUE /* Use capabilities instead */);
+		Validator.inRange(location, 0, Integer.MAX_VALUE /* TODO: Use capabilities instead */);
 		Validator.nonNegative(stride, "The stride in bytes can't be negative!");
 		Validator.nonNegative(offset, "The offset in bytes can't be negative!");
 		Validator.nonNegative(divisor, "The divisor can't be negative!");
 		Validator.positive(span, "The span must be strictly positive!");
-		
+
 		this.name = name;
 		this.bufferType = bufferType;
 		this.location = location;
@@ -67,88 +67,88 @@ public class VertexAttribute {
 		this.divisor = divisor;
 		this.span = span;
 	}
-	
+
 	@OpenGLCall
 	public void enable() {
 		assert location >= 0;
-		
-		if(useMultipleLocations()) {
-			for(int loc = location; loc < location + span; loc++) {
+
+		if (useMultipleLocations()) {
+			for (int loc = location; loc < location + span; loc++) {
 				GL33C.glEnableVertexAttribArray(loc);
 			}
 		} else {
 			GL33C.glEnableVertexAttribArray(location);
 		}
 	}
-	
+
 	@OpenGLCall
 	public void bindAttribute(VertexBuffer buffer) {
 		// First bind the concerned VBO.
 		buffer.bind();
-		
+
 		var type = buffer.getVertexBufferType();
 		var size = buffer.getSize();
-		var format = buffer.getFormat() == null ? 
-				type.getPreferredFormat() : buffer.getFormat();
+		var format = buffer.getFormat() == null ? type.getPreferredFormat() : buffer.getFormat();
 		// Normalized for floating-point data type isn't possible, disable it.
 		var normalized = format.isFloatingPoint() ? false : buffer.isNormalized();
-		
-		if(useMultipleLocations()) {
+
+		var stride = this.stride == 0 ? buffer.getStride() : this.stride;
+		var offset = this.offset == 0 ? buffer.getOffset() : this.offset;
+
+		if (useMultipleLocations()) {
 			var bytesPerData = size * format.getSizeInByte();
-			var stride = span * bytesPerData;
-			
-			for(int i = 0; i < span; i++) {
-				GL20C.glVertexAttribPointer(location + i, size, VertexBufferType.getOpenGLFormat(format), 
-						normalized, stride, i * bytesPerData);
+			var matStride = stride != 0 ? stride : span * bytesPerData;
+
+			for (int i = 0; i < span; i++) {
+				GL20C.glVertexAttribPointer(location + i, size, VertexBufferType.getOpenGLFormat(format), normalized,
+						matStride, stride + i * bytesPerData /* Initial stride + offset of location */);
 				GL33C.glVertexAttribDivisor(location + i, divisor);
 			}
 		} else {
-			var stride = this.stride == 0 ? buffer.getStride() : this.stride;
-			var offset = this.offset == 0 ? buffer.getOffset() : this.offset;
-			
-			GL20C.glVertexAttribPointer(location, size, VertexBufferType.getOpenGLFormat(format), normalized, stride, offset);
+			GL20C.glVertexAttribPointer(location, size, VertexBufferType.getOpenGLFormat(format), normalized, stride,
+					offset);
 			GL33C.glVertexAttribDivisor(location, divisor);
 		}
 	}
-	
+
 	@OpenGLCall
 	public void disable() {
 		assert location >= 0;
-		
-		if(useMultipleLocations()) {
-			for(int loc = location; loc < location + span; loc++) {
+
+		if (useMultipleLocations()) {
+			for (int loc = location; loc < location + span; loc++) {
 				GL33C.glDisableVertexAttribArray(loc);
 			}
 		} else {
 			GL33C.glDisableVertexAttribArray(location);
 		}
 	}
-	
+
 	/**
-	 * Return whether the <code>VertexAttributes</code> uses multiple vertex attribute location
-	 * in the shader file, meaning its span is greater than one.
+	 * Return whether the <code>VertexAttributes</code> uses multiple vertex
+	 * attribute location in the shader file, meaning its span is greater than one.
 	 * <p>
-	 * This is used when the vertex data passed to the shader is using more than 4 components such as
-	 * matrices (mat4 or mat3).
+	 * This is used when the vertex data passed to the shader is using more than 4
+	 * components such as matrices (mat4 or mat3).
 	 * 
 	 * @return Whether the attribute is spanning over multiple locations.
 	 */
 	private boolean useMultipleLocations() {
 		return span > 1;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getBufferType() {
 		return bufferType;
 	}
-	
+
 	@Override
 	public String toString() {
 		var buffType = (bufferType == null || bufferType.isEmpty()) ? "" : ", bufferType= " + bufferType;
-		return "Attribute { name= " + name + buffType + ", loc= " + location + ", stride= " + 
-				stride + ", offset= " + offset + ", div= " + divisor + ", span= " + span + " }";
+		return "Attribute { name= " + name + buffType + ", loc= " + location + ", stride= " + stride + ", offset= "
+				+ offset + ", div= " + divisor + ", span= " + span + " }";
 	}
 }
