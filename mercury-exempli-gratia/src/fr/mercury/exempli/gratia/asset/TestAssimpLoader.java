f8c95b88636706c93b45c54f6c94bb39ad27be75
GnosticOccultist <41146201+GnosticOccultist@users.noreply.github.com>
2023-02-23 22:05:44 +0100

    Added camera movement
    * Use LayeredInputProcessor to handle camera movements.

--- mercury-exempli-gratia/src/fr/mercury/exempli/gratia/asset/TestAssimpLoader.java
+++ mercury-exempli-gratia/src/fr/mercury/exempli/gratia/asset/TestAssimpLoader.java
@@ -2,6 +2,14 @@ package fr.mercury.exempli.gratia.asset;
 
 import fr.mercury.nucleus.application.MercuryApplication;
 import fr.mercury.nucleus.asset.loader.AssimpLoader;
+import fr.mercury.nucleus.input.Axis;
+import fr.mercury.nucleus.input.Button;
+import fr.mercury.nucleus.input.DelegateInputProcessor;
+import fr.mercury.nucleus.input.Input;
+import fr.mercury.nucleus.input.control.CameraControl;
+import fr.mercury.nucleus.input.layer.InputLayer;
+import fr.mercury.nucleus.input.layer.LayeredInputProcessor;
+import fr.mercury.nucleus.input.layer.LayeredInputProcessor.InputListener;
 import fr.mercury.nucleus.renderer.logic.state.BlendState;
 import fr.mercury.nucleus.renderer.logic.state.DepthBufferState;
 import fr.mercury.nucleus.renderer.logic.state.FaceCullingState;
@@ -17,12 +25,26 @@ import fr.mercury.nucleus.utils.ReadableTimer;
  * 
  * @author GnosticOccultist
  */
-public class TestAssimpLoader extends MercuryApplication {
+public class TestAssimpLoader extends MercuryApplication implements InputListener {
+    
+    public static final InputLayer CAM_LOOK_X = InputLayer.function("camera", "look-x");
+    
+    public static final InputLayer CAM_LOOK_Y = InputLayer.function("camera", "look-y");
+    
+    public static final InputLayer CAM_FOCUS = InputLayer.function("camera", "focus");
+    
+    public static final InputLayer CAM_MOVE = InputLayer.function("camera", "move");
+    
+    public static final InputLayer CAM_STRAFE = InputLayer.function("camera", "strafe");
 
     /**
      * The physica-mundi to represent the sponza model in the scene.
      */
     private AnimaMundi sponza;
+    /**
+     * The camera control.
+     */
+    private CameraControl camControl;
 
     /**
      * Launch method for the <code>TestAssimpLoader</code>, no arguments required.
@@ -37,6 +59,23 @@ public class TestAssimpLoader extends MercuryApplication {
     @Override
     @OpenGLCall
     protected void initialize() {
+        camControl = CameraControl.newFirstPersonControl(camera);
+        
+        var layeredInput = new LayeredInputProcessor();
+        getService(DelegateInputProcessor.class).setDelegate(layeredInput);
+        
+        // Activate the camera's input group.
+        layeredInput.activate(InputLayer.group("camera"));
+        
+        layeredInput.map(CAM_LOOK_X, Axis.MOUSE_X);
+        layeredInput.map(CAM_LOOK_Y, Axis.MOUSE_Y);
+        layeredInput.map(CAM_FOCUS, Button.MOUSE_LEFT);
+        layeredInput.map(CAM_MOVE, Input.Keys.KEY_W);
+        layeredInput.map(CAM_MOVE, -1, Input.Keys.KEY_S);
+        layeredInput.map(CAM_STRAFE, -1, Input.Keys.KEY_D);
+        layeredInput.map(CAM_STRAFE, Input.Keys.KEY_A);
+        layeredInput.listen(this, CAM_LOOK_X, CAM_LOOK_Y, CAM_FOCUS, CAM_MOVE, CAM_STRAFE);
+        
         // Load and prepare the cube in the scene.
         // TODO: Allow to add config flags when loading a model.
         sponza = assetManager.loadAnimaMundi("resources/model/sponza/sponza.gltf");
@@ -53,6 +92,33 @@ public class TestAssimpLoader extends MercuryApplication {
     @Override
     @OpenGLCall
     protected void update(ReadableTimer timer) {
+        camControl.update(timer);
+    }
 
+    @Override
+    public void trigger(InputLayer layer, double value) {
+        var input = getService(DelegateInputProcessor.class);
+        if (layer == CAM_LOOK_X && !input.isCursorVisible()) {
+            camControl.rotate(value, 0);
+        } 
+        if (layer == CAM_LOOK_Y && !input.isCursorVisible()) {
+            camControl.rotate(0, value);
+        }
+        if (layer == CAM_FOCUS) {
+            if (value == 1.0f) {
+                input.setCursorVisible(false);
+            } else {
+                input.setCursorVisible(true);
+            }
+        }
+        
+        
+        if (layer == CAM_MOVE) {
+            camControl.move(value, 0, 0);
+        }
+        
+        if (layer == CAM_STRAFE) {
+            camControl.move(0, 0, value);
+        }
     }
 }
--- mercury-nucleus/src/fr/mercury/nucleus/input/layer/LayeredInputProcessor.java
+++ mercury-nucleus/src/fr/mercury/nucleus/input/layer/LayeredInputProcessor.java
@@ -22,6 +22,10 @@ public class LayeredInputProcessor extends AbstractApplicationService implements
      */
     private final Set<InputLayer> activeLayers = new HashSet<>();
     /**
+     * The set of active input states.
+     */
+    private final Set<State> activeStates = new HashSet<>();
+    /**
      * The table containing listeners for each layer.
      */
     private final Map<InputLayer, Listeners> listenerMap = new HashMap<>();
@@ -33,54 +37,57 @@ public class LayeredInputProcessor extends AbstractApplicationService implements
      * The time per frame.
      */
     private volatile float tpf = 0;
-    
+
     /**
-     * Instantiates a new <code>LayeredInputProcessor</code> with {@link InputLayer#DEFAULT} active.
+     * Instantiates a new <code>LayeredInputProcessor</code> with
+     * {@link InputLayer#DEFAULT} active.
      */
     public LayeredInputProcessor() {
         // Activate the input default layer.
         activate(InputLayer.DEFAULT);
     }
-    
+
     /**
-     * Activate the provided {@link InputLayer} for the <code>LayeredInputProcessor</code>.
+     * Activate the provided {@link InputLayer} for the
+     * <code>LayeredInputProcessor</code>.
      * 
      * @param layer The layer to activate (not null).
-     * @return      Whether the layer was inactive before.
+     * @return Whether the layer was inactive before.
      */
     public boolean activate(InputLayer layer) {
         return activeLayers.add(layer);
     }
-    
+
     /**
-     * Deactivate the provided {@link InputLayer} for the <code>LayeredInputProcessor</code>.
+     * Deactivate the provided {@link InputLayer} for the
+     * <code>LayeredInputProcessor</code>.
      * 
      * @param layer The layer to deactivate (not null).
-     * @return      Whether the layer was active before.
+     * @return Whether the layer was active before.
      */
     public boolean deactivate(InputLayer layer) {
         return activeLayers.remove(layer);
     }
-    
+
     /**
-     * Return whether the provided {@link InputLayer} is active in the <code>LayeredInputProcessor</code>.
+     * Return whether the provided {@link InputLayer} is active in the
+     * <code>LayeredInputProcessor</code>.
      * 
      * @param layer The layer to check (not null).
-     * @return      Whether the layer is active.
+     * @return Whether the layer is active.
      */
     public boolean isLayerActive(InputLayer layer) {
         if (activeLayers.contains(layer)) {
             return true;
         }
-        
-        return activeLayers.stream()
-                .filter(l -> l.designateGroup() && l.group().equals(layer.group()))
-                .findAny().isPresent();
+
+        return activeLayers.stream().filter(l -> l.designateGroup() && l.group().equals(layer.group())).findAny()
+                .isPresent();
     }
-    
+
     /**
-     * Update the <code>LayeredInputProcessor</code> and the internal time per frame using 
-     * the specified {@link ReadableTimer}. 
+     * Update the <code>LayeredInputProcessor</code> and the internal time per frame
+     * using the specified {@link ReadableTimer}.
      * 
      * @param timer The readable only timer (not null).
      */
@@ -88,10 +95,16 @@ public class LayeredInputProcessor extends AbstractApplicationService implements
     @OpenGLCall
     public void update(ReadableTimer timer) {
         super.update(timer);
-        
+
         tpf = timer.getTimePerFrame();
+
+        for (var state : activeStates) {
+            var value = getStates(state.stateValue, false).lastValue;
+            state.updateValue(value);
+            notify(state.layer, state.lastValue);
+        }
     }
-    
+
     @Override
     public void mouseEvent(MouseEvent event) {
         if (event.getType() == MouseEvent.MOUSE_MOVED || event.getType() == MouseEvent.MOUSE_DRAGGED) {
@@ -99,21 +112,20 @@ public class LayeredInputProcessor extends AbstractApplicationService implements
                 // Notify about a X-axis mouse movement.
                 update(Axis.MOUSE_X, -event.getDX() / (1024.0 * tpf));
             }
-            
+
             if (event.getDY() != 0) {
                 // Notify about a Y-axis mouse movement.
                 update(Axis.MOUSE_Y, -event.getDY() / (1024.0 * tpf));
             }
         }
-        
-        if (event.getType() == MouseEvent.MOUSE_PRESSED || 
-                event.getType() == MouseEvent.MOUSE_RELEASED) {
+
+        if (event.getType() == MouseEvent.MOUSE_PRESSED || event.getType() == MouseEvent.MOUSE_RELEASED) {
             var button = Button.fromButtonIndex(event.getButton());
             var states = getStates(button, false);
             if (states == null) {
                 return;
             }
-            
+
             var value = event.getType() == MouseEvent.MOUSE_PRESSED ? 1.0 : 0.0;
             states.updateValue(value);
         }
@@ -121,133 +133,159 @@ public class LayeredInputProcessor extends AbstractApplicationService implements
 
     @Override
     public void keyEvent(KeyEvent event) {
-        if (event.getType() == KeyEvent.KEY_PRESSED || 
-                event.getType() == KeyEvent.KEY_RELEASED) {
-            var states = getStates(event.getCode(), false);
-            if (states == null) {
-                return;
-            }
-            
-            var value = event.getType() == KeyEvent.KEY_PRESSED ? 1.0 : 0.0;
-            states.updateValue(value);
+        var states = getStates(event.getCode(), false);
+        if (states == null) {
+            return;
         }
+
+        var value = (event.getType() == KeyEvent.KEY_PRESSED || event.getType() == KeyEvent.KEY_REPEATED) ? 1.0 : 0.0;
+
+        states.updateDelayed(value);
     }
 
     @Override
     public void dropEvent(String[] files) {
-        
+
     }
-    
+
     protected void notify(InputLayer layer, double value) {
         var listeners = getListeners(layer, false);
         if (listeners == null) {
             return;
         }
-        
+
         listeners.trigger(layer, value);
     }
-    
+
     public void listen(InputListener listener, InputLayer... layers) {
         for (var layer : layers) {
             var listeners = getListeners(layer, true);
             listeners.listeners.add(listener);
         }
     }
-    
+
     public void map(InputLayer layer, Axis axis) {
         addMapping(layer, 1.0, axis);
     }
-    
+
     public void map(InputLayer layer, Button button) {
         addMapping(layer, 1.0, button);
     }
-    
+
     public void map(InputLayer layer, int keyCode) {
         map(layer, 1.0, keyCode);
     }
-    
+
     public void map(InputLayer layer, double factor, int keyCode) {
         addMapping(layer, factor, keyCode);
     }
-    
+
     private void addMapping(InputLayer layer, double factor, Object stateValue) {
         var state = new State(layer, factor, stateValue);
         var states = getStates(stateValue, true);
         states.states.add(state);
     }
-    
+
     private Listeners getListeners(InputLayer layer, boolean add) {
         var listeners = listenerMap.get(layer);
         if (listeners == null && add) {
             listeners = new Listeners();
             listenerMap.put(layer, listeners);
         }
-        
+
         return listeners;
     }
-    
+
     private States getStates(Object state, boolean add) {
         var states = statesMap.get(state);
         if (states == null && add) {
             states = new States();
             statesMap.put(state, states);
         }
-        
+
         return states;
     }
-    
+
     protected boolean update(Axis axis, double value) {
         var states = getStates(axis, false);
         if (states == null) {
             return false;
         }
-        
+
         states.updateValue(value);
         return true;
     }
-    
+
     private class States {
-        
+
         final Array<State> states = Array.ofType(State.class);
+        double lastValue;
 
         void updateValue(double value) {
             for (var state : states) {
-                
+
                 var layer = state.layer;
                 if (!isLayerActive(layer)) {
                     continue;
                 }
-                
+
                 var listeners = getListeners(state.layer, false);
                 if (listeners == null) {
                     continue;
                 }
-                
+
                 // Apply the factor before triggering the value.
                 value *= state.factor;
-                
+
+                state.lastValue = value;
+
                 listeners.trigger(state.layer, value);
             }
         }
-        
+
+        void updateDelayed(double value) {
+            if (value == lastValue) {
+                return;
+            }
+
+            this.lastValue = value;
+
+            // Current state is triggered, so make it active.
+            if (lastValue != 0.0) {
+                activeStates.addAll(states);
+            } else {
+                activeStates.removeAll(states);
+            }
+        }
+
         @Override
         public String toString() {
             return states.toString();
         }
     }
-    
+
     private class State {
-        
+
         InputLayer layer;
+        double lastValue;
         double factor;
         Object stateValue;
-        
+
         public State(InputLayer layer, double factor, Object stateValue) {
             this.layer = layer;
             this.factor = factor;
             this.stateValue = stateValue;
         }
-        
+
+        void updateValue(double value) {
+            var scaled = value * factor;
+            if (lastValue == scaled) {
+                return;
+            }
+
+            this.lastValue = scaled;
+        }
+
         @Override
         public String toString() {
             return "State[layer= " + layer + ", state= " + stateValue + ", factor= " + factor + "]";
@@ -255,29 +293,30 @@ public class LayeredInputProcessor extends AbstractApplicationService implements
     }
 
     private class Listeners {
-        
+
         final Array<InputListener> listeners = Array.ofType(InputListener.class);
-        
+
         void trigger(InputLayer layer, double value) {
             for (var listener : listeners) {
                 listener.trigger(layer, value);
             }
         }
-        
+
         @Override
         public String toString() {
             return listeners.toString();
         }
     }
-    
+
     /**
-     * <code>InputListener</code> is an interface to implement listener for input mappings.
+     * <code>InputListener</code> is an interface to implement listener for input
+     * mappings.
      * 
      * @author GnosticOccultist
      */
     @FunctionalInterface
     public interface InputListener {
-        
+
         /**
          * Trigger the <code>InputListener</code> for the given {@link InputLayer}.
          * 
