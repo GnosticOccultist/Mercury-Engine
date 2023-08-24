209cd4ae9e525e1c5043ed8452f47cbab5f47573
GnosticOccultist <41146201+GnosticOccultist@users.noreply.github.com>
2023-08-24 15:14:57 +0200

    Missing ColorSpace class

---
+++ mercury-nucleus/src/fr/mercury/nucleus/texture/ColorSpace.java
@@ -0,0 +1,18 @@
+package fr.mercury.nucleus.texture;
+
+/**
+ * <code>ColorSpace</code> enumerates all supported colorspace for an {@link Image}.
+ * 
+ * @author GnosticOccultist
+ */
+public enum ColorSpace {
+
+    /**
+     * The linear color space.
+     */
+    LINEAR,
+    /**
+     * The sRGB color space.
+     */
+    sRGB;
+}
